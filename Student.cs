bace371224214016ebad9151cebf6c3f9a1a99b2
Séamus Hirrell <seamushirrell@gmail.com>
2022-12-02 21:03:57 +0000

    added input validation removed id from list view

--- Student.cs
+++ Student.cs
@@ -1,19 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace assignment_2;
 
 public partial class Student
 {
-    public int Id { get; set; }
+  public int Id { get; set; }
 
-    public string FirstName { get; set; } = null!;
+  [DataType(DataType.Text)]
+  [Required(ErrorMessage = "Please enter a first name")]
+  public string FirstName { get; set; } = null!;
 
-    public string LastName { get; set; } = null!;
+  [DataType(DataType.Text)]
+  [Required(ErrorMessage = "Please enter a last name")]
+  public string LastName { get; set; } = null!;
 
-    public string EmailAddress { get; set; } = null!;
+  [DataType(DataType.EmailAddress)]
+  [Required(ErrorMessage = "Please enter an email address")]
+  public string EmailAddress { get; set; } = null!;
 
-    public string PhoneNumber { get; set; } = null!;
+  [DataType(DataType.PhoneNumber)]
+  [Required(ErrorMessage = "Please enter a phone number")]
+  [RegularExpression(@"^(\d{3}-\d{3}-\d{4})$", ErrorMessage = "Please enter a valid phone number (xxx-xxx-xxxx)")]
+  public string PhoneNumber { get; set; } = null!;
 
-    public string Country { get; set; } = null!;
+  [DataType(DataType.Text)]
+  [Required(ErrorMessage = "Please enter a country")]
+  public string Country { get; set; } = null!;
 }
